26d8b6780e9e016a71f19a18121a4294204f8178
hongs429 <honshyeon@gmail.com>
2025-07-27 14:04:42 +0900

    Request Rate Limiter 적용하기 + Redis-reactive radis-session 적용 기본 배포구성

--- src/test/java/com/polarbookshop/edgeservice/EdgeServiceApplicationTests.java
+++ src/test/java/com/polarbookshop/edgeservice/EdgeServiceApplicationTests.java
@@ -2,9 +2,31 @@ package com.polarbookshop.edgeservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
 
-@SpringBootTest
+@SpringBootTest( // 완전한 스프링 웹 애플리케이션 컨텍스트와 임의의 포트에서 듣는 웹 환경을 로드한다
+        webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT
+)
+@Testcontainers
 class EdgeServiceApplicationTests {
+
+    private static final int REDIS_PORT = 6379;
+
+    @Container
+    static GenericContainer<?> redis = new GenericContainer<>(DockerImageName.parse("redis:7.2"))
+            .withExposedPorts(REDIS_PORT);
+
+
+    @DynamicPropertySource
+    static void properties(DynamicPropertyRegistry registry) {
+        registry.add("spring.data.redis.host", redis::getHost);
+        registry.add("spring.data.redis.port", ()->redis.getMappedPort(REDIS_PORT));
+    }
 
     @Test
     void contextLoads() {
